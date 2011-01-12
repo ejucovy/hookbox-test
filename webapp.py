0f60e8dcfd74c25a05feaa2f37253c51f4e504b4
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2011-01-12 20:35:14 +0000

    * Persist location data in a mongodb * Load divs on pageload in persistent locations -- templatize index.html * Add click-to-add new divs

--- webapp.py
+++ webapp.py
@@ -1,6 +1,10 @@
 import json
 from webob import Request, Response
 from paste.httpserver import serve
+from pymongo import Connection
+from tempita import Template
+
+data = dict()
 
 class App(object):
 
@@ -16,11 +20,15 @@ class App(object):
         except AttributeError:
             return Response('404', status=404)(environ, start_response)
         resp = Response(json.dumps(method(req)), content_type="application/json")
-        print resp.body
         return resp(environ, start_response)
 
     def index(self, req):
-        _index = open('index.html').read()
+        _index = Template.from_filename('index.html')
+        conn = Connection()
+        db = conn.comet
+        coll = db.points
+        data = coll.find()
+        _index = _index.substitute(data=data)
         return _index
 
     def connect(self, req):
@@ -30,7 +38,7 @@ class App(object):
         return [True, {}]
 
     def create_channel(self, req):
-        return [True, {'history_size': 0, "reflective": True, "presenceful": True}]
+        return [True, {'history_size': 0, "reflective": False, "presenceful": True}]
 
     def subscribe(self, req):
         return [True, {}]
@@ -39,6 +47,24 @@ class App(object):
         return [True, {}]
 
     def publish(self, req):
+        # some stupid mangling is required
+        data = req.POST['payload'].replace("'", '"').replace('u"', '"')
+        data = json.loads(data)
+        id_ = data['id']
+        conn = Connection()
+        db = conn.comet
+        coll = db.points
+        coll.update({u"id": id_}, {"$set": data}, upsert=True)
         return [True, {}]
 
-serve(App(), port="8080")
+    def getpoints(self, req):
+        conn = Connection()
+        db = conn.comet
+        coll = db.points
+        x = []
+        for i in coll:
+            del i['_id']
+            x.append(i)
+        return x
+
+serve(App(), host="0.0.0.0", port="4444")
